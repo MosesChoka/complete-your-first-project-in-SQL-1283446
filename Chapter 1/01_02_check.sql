7feaf1d86a4c1ac5aca8b8f6a7e0e44b903ad0f5
Moses Choka <89992068+MosesChoka@users.noreply.github.com>
2024-03-22 15:02:15 +0000

    Finished chapter one

--- Chapter 1/01_02_check.sql
+++ Chapter 1/01_02_check.sql
@@ -1,2 +1,4 @@
 -- View data in Customer table
 SELECT * FROM Customer;
+
+
--- Chapter 2/02_01b.sql
+++ Chapter 2/02_01b.sql
@@ -1,1 +1,6 @@
 -- Find how many products sold
+SELECT 
+COUNT(DISTINCT(ProductID))
+AS TotalUniqueProducts,
+SUM(Quantity)AS TotalQuantity
+FROM OrderItem;
