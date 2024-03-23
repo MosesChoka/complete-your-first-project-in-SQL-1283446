a8753488f51f4b0c96e0acbf1c8cb8e87c952c7c
Moses Choka <89992068+MosesChoka@users.noreply.github.com>
2024-03-23 11:43:59 +0000

    Sales by month and year

--- Chapter 2/02_06b.sql
+++ Chapter 2/02_06b.sql
@@ -1,1 +1,12 @@
 -- Find sales by month and year
+SELECT 
+MONTHNAME(CreationDate) as MonthName,
+YEAR(CreationDate) as OrderYear,
+COUNT(Orders.OrderID) as TotalOrders,
+SUM(Quantity) AS TotalQuantity,
+SUM(TotalDue) as TotalAmount
+From Orders
+LEFT OUTER JOIN OrderItem
+ON Orders.OrderID = OrderItem.OrderID
+GROUP BY MonthName, OrderYear
+ORDER BY OrderYear, MONTH(CreationDate);
